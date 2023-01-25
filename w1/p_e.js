4891d10f1c25886087c4d50bea5a15187f836f31
Shawn Ogbomo <Shawn_Ogbomo@hotmail.com>
2023-01-25 18:34:41 -0500

    Update p_e.js

--- w1/p_e.js
+++ w1/p_e.js
@@ -2,7 +2,7 @@
 let label = "senecacollege";
 let tld = "ca";
 let domainName = label + "." + tld;
-console.log( domainName);
+console.log(domainName);
 
 
 //question 2
@@ -29,4 +29,55 @@ let ipAddress = byte1.toString() + "." + byte2.toString() + "." + byte3.toString
 console.log(ipAddress);
 
 //question 7
-let ipInt
+let ipInt = ((byte1 + byte2 + byte3 + byte4).toString(2));
+
+
+//let result =  parseInt(ipInt,2);
+//console.log(result);
+
+
+//question 8
+
+
+//question 9
+const imATeapot = 418;
+let statusCode = imATeapot;
+console.log(statusCode);
+
+//question 10
+const fourxxMin = 400;
+const fourxxMax = 451;
+if (statusCode >= fourxxMin && statusCode <= fourxxMax) {
+    console.log("Status code: " + statusCode + " is a 4xx client error...");
+}
+
+//question 11
+const cont = 100;
+const switchingProtocols = 101;
+const processing = 102;
+const earlyHints = 103;
+switch (statusCode) {
+    case cont:
+        console.log(statusCode + " continue");
+        break;
+    case switchingProtocols:
+        console.log(statusCode + " switching protocols");
+        break;
+    case processing:
+        console.log(status + " processing");
+        break;
+    case earlyHints:
+        console.log(statusCode + " early hints");
+        break;
+    default:
+        console.log("Status code: " + statusCode + " is unknown...");
+}
+
+//question 12
+function is2xx(status) {
+    const twoxxstatusMin = 200;
+    const twoxxstatusMax = 226;
+    return status >= twoxxstatusMin && status <= twoxxstatusMax;
+}
+
+console.log(is2xx(statusCode));
