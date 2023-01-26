a4400ec7a823cf27354d6558130cd961cd6beabd
Shawn Ogbomo <Shawn_Ogbomo@hotmail.com>
2023-01-26 17:45:43 -0500

    Update w2e.js

--- w2/w2e.js
+++ w2/w2e.js
@@ -19,3 +19,52 @@ try {
 }
 
 //question 2
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function roll(sides) {
+    if (sides <= 0 || typeof sides == "string") {
+        throw Invalid;
+    } else if (!sides) {
+        sides = 6;
+    }
+    if (sides) {
+        return getRandomIntInclusive(1, sides);
+    }
+}
+
+try {
+    console.log(roll(53.54));
+} catch (Invalid) {
+    console.log("oops, something went wrong...");
+}
+
+//question 3 / 4
+const farenheit = 32;
+
+function celciusToFarenheit(temp, delimiter) {
+    let absoluteZero = -7777;
+    delimiter = delimiter.toUpperCase();
+    const celcius = "C";
+    const farenheit = "F";
+    if (temp === absoluteZero || typeof temp == "string" || (delimiter !== celcius && delimiter !== farenheit)) {
+        throw Invalid;
+    }
+    if (delimiter === celcius) {
+        return ((temp * (9 / 5)) + 32).toPrecision(5);
+    } else if (delimiter === farenheit) {
+        return (temp - 32) * (5 / 9).toPrecision(5);
+    }
+}
+
+try {
+    console.log(celciusToFarenheit(-22, "c"));
+} catch (Invalid) {
+    console.log("oops, something went wrong...");
+}
+
+
+//question 5
